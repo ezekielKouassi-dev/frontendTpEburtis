18ad6c1eea9d9f4f46ffb12e83ac12363b47122a
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-04-24 16:42:00 +0000

    menu test

--- src/app/core/components/shared/menu-bar/menu-bar.component.spec.ts
+++ src/app/core/components/shared/menu-bar/menu-bar.component.spec.ts
@@ -1,8 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MenuBarComponent } from './menu-bar.component';
-import {MegaMenuModule} from "primeng/megamenu";
-import {ActivatedRoute, RouterModule, Routes} from "@angular/router";
 import {PrimengModule} from "../../../modules/shared/primeng.module";
 import {AppRoutingModule} from "../../../../app-routing.module";
 
